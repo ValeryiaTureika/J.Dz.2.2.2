47eb8ad46e7ea837a7ebc99994d6bf188a77bf97
Valeria Hurava <ler4ik65@gmail.com>
2021-09-24 21:46:59 +0200

    modification commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate(int BodyWeightInKg, int HeightInCentimeters, int TransferToKg) {
-        int BodyMassIndex = (BodyWeightInKg *TransferToKg) / (HeightInCentimeters * HeightInCentimeters) ;
-        return BodyMassIndex;
+    public int calculate(int bodyWeightInKg, int heightInCentimeters, int transferToKg) {
+        int bodyMassIndex = (bodyWeightInKg * transferToKg) / (heightInCentimeters * heightInCentimeters);
+        return bodyMassIndex;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,22 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
 
-        int BodyMassIndex = service.calculate(80, 165, 10_000);
-        System.out.println("Индекс Массы Тела: " + BodyMassIndex + "кг/м;  " + "Вес: 80 кг;  " + "Рост: 1,65 м");
+        int bodyMassIndex = service.calculate(80, 165, 10_000);
+        System.out.println("Индекс Массы Тела: " + bodyMassIndex + "кг/м;  " + "Вес: 80 кг;  " + "Рост: 1,65 м");
 
-        int BodyMassIndex1 = service.calculate(100, 185, 10_000);
-        System.out.println("Индекс Массы Тела: " + BodyMassIndex1 + "кг/м;  " + "Вес: 100 кг;  " + "Рост: 1,85 м");
+        int bodyMassIndex1 = service.calculate(100, 185, 10_000);
+        System.out.println("Индекс Массы Тела: " + bodyMassIndex1 + "кг/м;  " + "Вес: 100 кг;  " + "Рост: 1,85 м");
 
-        int BodyMassIndex2 = service.calculate(54, 160, 10_000);
-        System.out.println("Индекс Массы Тела: " + BodyMassIndex2 + "кг/м;  " + "Вес: 54 кг;  " + "Рост: 1,60 м");
+        int bodyMassIndex2 = service.calculate(54, 160, 10_000);
+        System.out.println("Индекс Массы Тела: " + bodyMassIndex2 + "кг/м;  " + "Вес: 54 кг;  " + "Рост: 1,60 м");
 
-        int BodyMassIndex3 = service.calculate(90, 186, 10_000);
-        System.out.println("Индекс Массы Тела: " + BodyMassIndex3 + "кг/м;  " + "Вес: 90 кг;  " + "Рост: 1,86 м");
+        int bodyMassIndex3 = service.calculate(90, 186, 10_000);
+        System.out.println("Индекс Массы Тела: " + bodyMassIndex3 + "кг/м;  " + "Вес: 90 кг;  " + "Рост: 1,86 м");
 
-        int BodyMassIndex4 = service.calculate(105, 197, 10_000);
-        System.out.println("Индекс Массы Тела: " + BodyMassIndex4 + "кг/м;  " + "Вес: 105 кг;  " + "Рост: 1,97 м");
+        int bodyMassIndex4 = service.calculate(105, 197, 10_000);
+        System.out.println("Индекс Массы Тела: " + bodyMassIndex4 + "кг/м;  " + "Вес: 105 кг;  " + "Рост: 1,97 м");
 
-        int BodyMassIndex5 = service.calculate(75, 165, 10_000);
-        System.out.println("Индекс Массы Тела: " + BodyMassIndex5 + "кг/м;  " + "Вес: 75 кг;  " + "Рост: 1,65 м");
+        int bodyMassIndex5 = service.calculate(75, 165, 10_000);
+        System.out.println("Индекс Массы Тела: " + bodyMassIndex5 + "кг/м;  " + "Вес: 75 кг;  " + "Рост: 1,65 м");
     }
 }
