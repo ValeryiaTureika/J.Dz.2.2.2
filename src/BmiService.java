2792e4789fccf1e0a76dceef2118661ef60de2ff
Valeria Hurava <ler4ik65@gmail.com>
2021-09-27 12:20:25 +0200

    replaced the name of the variables in the formula for calculating the body mass index

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate(int bodyWeightInKg, int heightInCentimeters, int transferToKg) {
-        int bodyMassIndex = (bodyWeightInKg * transferToKg) / (heightInCentimeters * heightInCentimeters);
+    public int calculate(int bodyWeightInKg, int heightInCentimetersInCm, int transferToM) {
+        int bodyMassIndex = (bodyWeightInKg * transferToM) / (heightInCentimetersInCm * heightInCentimetersInCm);
         return bodyMassIndex;
     }
 }
