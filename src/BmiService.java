c28d646b5c25f339d5f3669ca07fb1c7094de6c6
Valeria Hurava <ler4ik65@gmail.com>
2021-09-30 16:28:36 +0200

    removed the parameter from the multiplier

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate(int bodyWeightInKg, int heightInCentimetersInCm, int transferToM) {
-        int bodyMassIndex = (bodyWeightInKg * transferToM) / (heightInCentimetersInCm * heightInCentimetersInCm);
+    public int calculate(int bodyWeightInKg, int heightInCentimetersInCm) {
+        int bodyMassIndex = (bodyWeightInKg * 10_000) / (heightInCentimetersInCm * heightInCentimetersInCm);
         return bodyMassIndex;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,22 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
 
-        int bodyMassIndex = service.calculate(80, 165, 10_000);
+        int bodyMassIndex = service.calculate(80, 165);
         System.out.println("Индекс Массы Тела: " + bodyMassIndex + "кг/м;  " + "Вес: 80 кг;  " + "Рост: 1,65 м");
 
-        int bodyMassIndex1 = service.calculate(100, 185, 10_000);
+        int bodyMassIndex1 = service.calculate(100, 185);
         System.out.println("Индекс Массы Тела: " + bodyMassIndex1 + "кг/м;  " + "Вес: 100 кг;  " + "Рост: 1,85 м");
 
-        int bodyMassIndex2 = service.calculate(54, 160, 10_000);
+        int bodyMassIndex2 = service.calculate(54, 160);
         System.out.println("Индекс Массы Тела: " + bodyMassIndex2 + "кг/м;  " + "Вес: 54 кг;  " + "Рост: 1,60 м");
 
-        int bodyMassIndex3 = service.calculate(90, 186, 10_000);
+        int bodyMassIndex3 = service.calculate(90, 186);
         System.out.println("Индекс Массы Тела: " + bodyMassIndex3 + "кг/м;  " + "Вес: 90 кг;  " + "Рост: 1,86 м");
 
-        int bodyMassIndex4 = service.calculate(105, 197, 10_000);
+        int bodyMassIndex4 = service.calculate(105, 197);
         System.out.println("Индекс Массы Тела: " + bodyMassIndex4 + "кг/м;  " + "Вес: 105 кг;  " + "Рост: 1,97 м");
 
-        int bodyMassIndex5 = service.calculate(75, 165, 10_000);
+        int bodyMassIndex5 = service.calculate(75, 165);
         System.out.println("Индекс Массы Тела: " + bodyMassIndex5 + "кг/м;  " + "Вес: 75 кг;  " + "Рост: 1,65 м");
     }
 }
